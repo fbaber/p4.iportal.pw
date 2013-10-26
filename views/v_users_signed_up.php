9b007afba50efbbbaccd55e0cf0ea6dd746a340c
Furrukh Baber <furrukh.baber@gmail.com>
2013-10-26 12:44:59 -0400

    fixed errors with controllers

--- views/v_users_signed_up.php
+++ views/v_users_signed_up.php
@@ -3,10 +3,5 @@
 <body>
 	<p><b>You have signed up</b>
 	</p>
-	<frame>
-	<?php $this->template->content = View::instance('v_users_login'); ?>
-     	<?php $this->template->content->error = $error; ?>
-		<?php echo $this->template; ?>
-	</frame>
 </body>
 </html>
